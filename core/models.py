82e3194e6ecd2f5aa95b876772496776beee00f3
Jordi Lucas <jordilucas3@gmail.com>
2019-07-01 14:35:51 -0300

    bug

--- core/models.py
+++ core/models.py
@@ -7,11 +7,11 @@ from localizacao.models import Localizacao
 class PontoTuristico(models.Model):
     nome =  models.CharField(max_length=150)
     descricao = models.TextField()
-    aprovado = models.BooleanField()
+    aprovado = models.BooleanField(default=False)
     atracao = models.ManyToManyField(Atracao)
     comentarios = models.ManyToManyField(Comentarios)
     avaliacoes = models.ManyToManyField(Avalicaoes)
-    localizacao = models.ForeignKey(Localizacao, on_delete=models.CASCADE)
+    localizacao = models.ForeignKey(Localizacao, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.nome
