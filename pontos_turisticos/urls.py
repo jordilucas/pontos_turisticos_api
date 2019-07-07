d7e41c63967bafe3523710786c48199289a60240
Jordi Lucas <jordilucas3@gmail.com>
2019-07-07 09:03:24 -0300

    Endpoint de comentarios

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -4,9 +4,12 @@ from django.conf.urls import include
 from rest_framework import routers
 from core.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewsets import AtracoesViewSet
+from comentarios.api.viewsets import ComentariosViewSet
+
 router = routers.DefaultRouter()
 router.register(r'pontoturisticos', PontoTuristicoViewSet)
 router.register(r'atracoes', AtracoesViewSet)
+router.register(r'comentarios', ComentariosViewSet)
 
 
 urlpatterns = [
