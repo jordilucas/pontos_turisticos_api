f3864b2bb9c754d238e64d10df4ac86aea69ec9f
Jordi Lucas <jordilucas3@gmail.com>
2019-07-10 11:13:11 -0300

    Endpoint que retorna dados completos de ponto turistico

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework.serializers import ModelSerializer
 from core.models import PontoTuristico
+from atracoes.api.serializers import AtracaoSerializer 
+from comentarios.api.serializers import ComentariosSerializer
+from avaliacoes.api.serializers import AvaliacoesSerializer
+from localizacao.api.serializers import EnderecosSerializer
 
 class PontoTuristicoSerializer(ModelSerializer):
+    atracao = AtracaoSerializer(many=True)
+    comentarios = ComentariosSerializer(many=True)
+    avaliacoes = AvaliacoesSerializer(many=True)
+    localizacao = EnderecosSerializer()
+
     class Meta:
         model = PontoTuristico
-        fields = ('id', 'nome', 'descricao','foto')#,'aprovado',
-                  #'atracao','comentarios','avaliacoes','localizacao')
+        fields = ('id', 'nome', 'descricao','foto','aprovado',
+                  'atracao','comentarios','avaliacoes','localizacao')
