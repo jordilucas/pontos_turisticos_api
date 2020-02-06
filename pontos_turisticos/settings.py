ecfaacb0bd2abb960d4a80fce9e6fc55f5984c90
Jordi <jordilucas3@gmail.com>
2020-02-06 15:27:37 -0300

    comments

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -137,12 +137,12 @@ MEDIA_ROOT = 'imagens'
 
 MEDIA_URL = '/media/'
 
-DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
-GS_BUCKET_NAME = 'pontosturisticos'
-STATICFILES_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
-GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
-    "/pontos-turisticos-267315-0498fd913ee9.json"
-)
+#DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
+#GS_BUCKET_NAME = 'pontosturisticos'
+#STATICFILES_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
+#GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
+#    "pontos-turisticos-267315-0498fd913ee9.json"
+#)
 #MEDIA_URL = 'django_gcloud_storage.DjangoGCloudStorage'
 
 #GOOGLE_CLOUD_STORAGE_BUCKET = '/pontosturisticos' # the name of the bucket you have created from the google cloud storage console
